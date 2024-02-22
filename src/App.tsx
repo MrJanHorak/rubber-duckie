c4a43c1005258069a4f5959fa09d01ffe2e9a98d
MrJanHorak <janny_b_goode@hotmail.com>
2024-02-21 21:45:39 -0500

    add conversation state

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
-import React, { useState } from 'react';
+import { useState } from 'react'; 
+import { v4 as uuidv4 } from 'uuid';
+
+// components
 import Header from './components/Header/Header';
 import ChatWindow from './components/ChatWindow/ChatWindow';
 import InputArea from './components/InputArea/InputArea';
 import RubberDuck from './components/RubberDuck/RubberDuck';
 import ResponseArea from './components/ResponseArea/ResponseArea';
 
+//styles
 import './App.css';
+
+//types
 import { Conversation } from './types/types';
 
-const conversation: Conversation = [
+const generarteUniqueId = () => {
+  return uuidv4();
+}
+
+const startConversation: Conversation = [
   {
     text: 'Hello, I am a rubber duck. How can I help you?',
     user: 'rubber-duck',
@@ -16,15 +26,34 @@ const conversation: Conversation = [
   },
 ];
 
-const handleUserInput = (input: string) => {
-  console.log(input);
-};
 
-const handleSpeechInput = (input: string) => {
-  console.log(input);
-};
 
 function App() {
+  const [conversation, setConversation] = useState(startConversation); 
+  
+  const handleUserInput = (input: string) => {
+    const newMessage = {
+      id: generarteUniqueId(),
+      text: input,
+      sender: 'user',
+      timestamp: Date.now(),
+    };
+    setConversation((prevConversation) => [...prevConversation, newMessage]); // Add setConversation function
+    console.log(input);
+  };
+  
+  const handleSpeechInput = (input: string) => {
+    const newMessage = {
+      id: generarteUniqueId(),
+      text: input,
+      sender: 'user',
+      timestamp: Date.now(),
+    };
+    setConversation((prevConversation: Conversation[]) => [...prevConversation, newMessage]); //
+    console.log(input);
+  };
+  // Declare conversation and setConversation using useState hook
+
   return (
     <div className='App'>
       <Header />
